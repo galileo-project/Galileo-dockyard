ccc9560131246d7f01dfc1e6753f50997c6b9cd8
ColeChan <chenwenjie27@gmail.com>
2016-04-27 18:46:36 +0800

    Add log mod

---
+++ dockyard/dockyard/model/logs/__init__.py
@@ -0,0 +1,18 @@
+class Log:
+    def warn(self, msg):
+        pass
+
+    def fatal(self, msg):
+        pass
+
+    def put(self, msg):
+        pass
+
+    def success(self, msg):
+        pass
+
+    def error(self, msg):
+        pass
+
+    def raw(self, msg):
+        pass
---
+++ dockyard/dockyard/model/logs/sys.py
@@ -0,0 +1,5 @@
+from dockyard.utils.mongo import Mongo
+from dockyard.model.logs import Log
+
+class SysLog(Mongo, Log):
+    pass
