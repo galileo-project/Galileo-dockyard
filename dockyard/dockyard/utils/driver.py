2b553fb8556d8949b7714c7e3a5cb57a49479075
ColeChan <chenwenjie27@gmail.com>
2016-05-19 18:00:45 +0800

    Add driver interface

--- dockyard/dockyard/utils/driver.py
+++ dockyard/dockyard/utils/driver.py
@@ -1,3 +1,16 @@
+from dockyard.const import APIStatus
+
 class Driver:
-    def __init__(self):
-        pass
+    def _export(self, msg, stat=None):
+        if stat is None:
+            return False, msg
+        else:
+            return True, stat
+
+    def success(self, msg=None):
+        return self._export(msg)
+
+    def error(self, stat=None):
+        if stat is None:
+            stat = APIStatus["STAT_API_UNKNOWN_ERROR"]
+        return self._export(None, stat)
