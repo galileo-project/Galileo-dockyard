6732ad6e2f40b825313a21dbf896c030c8176ee0
ColeChan <chenwenjie27@gmail.com>
2016-05-19 16:27:59 +0800

    Update routine

--- dockyard/dockyard/service/routine/__init__.py
+++ dockyard/dockyard/service/routine/__init__.py
@@ -1,3 +1,27 @@
+from dockyard.var import GLOBAL
+
+class _Routine:
+    KEY = None
+
+    def __init__(self):
+        GLOBAL.mq.subscribe(self.KEY, self)
+
+    def callback(self, msg):
+        try:
+            ret = self._exec(msg)
+            if ret:
+                msg.received(self.KEY)
+        except:
+            pass
+
+    def resume(self):
+        raise NotImplemented
+
+    def _exec(self, msg):
+        raise NotImplemented
+
+
+#init routine
 import pkgutil
 
 def init():
--- dockyard/dockyard/service/routine/_routine_log.py
+++ dockyard/dockyard/service/routine/_routine_log.py
@@ -1,12 +1,11 @@
-from dockyard.service.routine._default import _Routine
+from dockyard.service.routine import _Routine
 from dockyard.var import GLOBAL
 
 class __RoutineLog(_Routine):
     KEY = GLOBAL.CHAN_LOG
 
-    def callback(self, msg):
-        #do something
-        msg.received(self.KEY)
+    def _exec(self, msg):
+        pass
 
     def resume(self):
         pass
