b31da1d57543ee652744003debf4d98372b8855f
ColeChan <chenwenjie27@gmail.com>
2016-05-23 15:35:41 +0800

    Update task queue use go

--- server/dockyard/service/task/queue.py
+++ server/dockyard/service/task/queue.py
@@ -1,5 +1,4 @@
 from dockyard.var import GLOBAL
-from tornado.ioloop import IOLoop
 from dockyard.driver.task import Task
 
 
@@ -10,7 +9,7 @@ class TaskQueue:
     def send(self, channel, expire=-1, **kwargs):
         err, task = Task().add(channel, kwargs, expire)
         callback = self.__subscriber[channel].callback
-        IOLoop.instance().add_callback(callback, task)
+        GLOBAL.go(callback, task)
 
     def broadcast(self, expire=-1, **kwargs):
         err, task = Task().add(GLOBAL.CHAN_GLOBAL, kwargs, expire)
@@ -26,4 +25,4 @@ class TaskQueue:
 
     def __broadcast(self, task):
         for subscriber in self.__subscriber.values():
-            IOLoop.instance().add_callback(subscriber.callback, task)
+            GLOBAL.go(subscriber.callback, task)
