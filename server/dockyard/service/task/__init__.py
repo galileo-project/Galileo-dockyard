06de33ee699a3ca1a73a98edf1590ad6dab1a807
ColeChan <chenwenjie27@gmail.com>
2016-05-23 17:04:11 +0800

    Rename task to task queue

--- server/dockyard/service/task/__init__.py
+++ server/dockyard/service/task/__init__.py
@@ -1,3 +1,3 @@
 from dockyard.service.task._queue import init as init_queue
 from dockyard.service.task._routines import init as init_routine
-from dockyard.service.task._task import TaskQueue
+from dockyard.service.task._task_queue import TaskQueue
