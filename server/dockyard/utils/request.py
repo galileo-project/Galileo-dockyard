5be3710c09cacdd359c2d2fb01f6d98a71a8140b
ColeChan <chenwenjie27@gmail.com>
2016-06-07 18:13:34 +0800

    Fix request

--- server/dockyard/utils/request.py
+++ server/dockyard/utils/request.py
@@ -12,12 +12,12 @@ class Request(object):
 
     def get(self, url, data=None):
         GLOBAL.puts("Request: ", data)
-        response = requests.get(url=url, params=data, hearders=self.__headers)
+        response = requests.get(url=url, params=data, headers=self.__headers)
         return self.__parse(response)
 
     def post(self, url, data):
         GLOBAL.puts("Request: ", data)
-        response = requests.post(url, data=data, hearders=self.__headers)
+        response = requests.post(url, data=data, headers=self.__headers)
         return self.__parse(response)
 
     def put(self):
